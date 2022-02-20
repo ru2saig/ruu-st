cd714e028bb4f86fa6c9148e2a5d3b528c23ded2
ru2saig <42857541+ru2saig@users.noreply.github.com>
2022-02-20 21:01:13 +0530

    Set _NET_WM_ICON_(CARDINAL) to Adwaita 32x32 utilities-terminal.png

--- x.c
+++ x.c
@@ -94,7 +94,7 @@ typedef struct {
 	Window win;
 	Drawable buf;
 	GlyphFontSpec *specbuf; /* font spec buffer used for rendering */
-	Atom xembed, wmdeletewin, netwmname, netwmiconname, netwmpid;
+        Atom xembed, wmdeletewin, netwmname, netwmiconname, netwmpid, netwmicon;
 	struct {
 		XIM xim;
 		XIC xic;
@@ -1231,6 +1231,10 @@ xinit(int cols, int rows)
 	XChangeProperty(xw.dpy, xw.win, xw.netwmpid, XA_CARDINAL, 32,
 			PropModeReplace, (uchar *)&thispid, 1);
 
+        xw.netwmiconname = XInternAtom(xw.dpy, "_NET_WM_ICON", False);
+	XChangeProperty(xw.dpy, xw.win, xw.netwmiconname, XA_CARDINAL, 32,
+		        PropModeReplace, (uchar *) icon_buffer, icon_buffer_length);	     
+		
 	win.mode = MODE_NUMLOCK;
 	resettitle();
 	xhints();
