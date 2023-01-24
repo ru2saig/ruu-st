af8532f8a9f6e7245cd5038e8eccd89dfa115778
ru2saig <42857541+ru2saig@users.noreply.github.com>
2023-01-24 11:49:48 +0530

    Changed to Fira Code Nerd font

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 #include <X11/X.h>
-static char *font = "Fira Code Medium:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Fira Code Nerd Font :pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*
